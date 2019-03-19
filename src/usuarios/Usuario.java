806ff4a6b5609319d8a45b7ccb3fe059711c555f
luismarioram99 <luismarioram99@gmail.com>
2019-03-19 09:55:45 -0600

    Creación del objeto usuario

---
+++ src/usuarios/Usuario.java
@@ -0,0 +1,33 @@
+package usuarios;
+public class Usuario {
+    
+    private String user;
+    private String password;
+    
+    public Usuario(String user, String password){
+    
+        this.user = user;
+        this.password = password;
+    
+    }
+    
+    public String getUser(){ return this.user;}
+    
+    public boolean checkPassword(String pass){
+        
+        return this.password.equals(pass);
+              
+    }
+    
+    public void changePassword(String pass, String newpass){
+    
+        if(!checkPassword(pass))
+            return;
+        
+        this.password = newpass;
+    
+    }
+ 
+    
+}
+
