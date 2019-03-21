cf6299868acc1b85652f0481025a225dab7eae34
luismarioram99 <luismarioram99@gmail.com>
2019-03-20 23:29:08 -0600

    Revert "cambios finales Esto se va amiguitos"
    This reverts commit c60a1fafe5ddfe53ba038e31fc64cbde4d234fa9.

--- src/formularios/bienvenida.java
+++ src/formularios/bienvenida.java
@@ -5,9 +5,6 @@
  */
 package formularios;
 
-import java.awt.Dimension;
-import java.awt.Toolkit;
-
 /**
  *
  * @author Jose Carlos
@@ -19,21 +16,10 @@ public class bienvenida extends javax.swing.JFrame {
      */
     
     String user;
-    
-    private String getUser(){return user;}
-    
     public bienvenida(String user){
         this.user = user;
-        bienvenida.super.setUndecorated(true);
-        Dimension pantalla = Toolkit.getDefaultToolkit().getScreenSize();
-        int height = pantalla.height;
-        int width = pantalla.width;
-        setSize(width / 2, height / 2);
-        setLocationRelativeTo(null);
-        
+        modify(user);
         initComponents();
-        
-        
     }
 
     /**
@@ -49,11 +35,8 @@ public class bienvenida extends javax.swing.JFrame {
         Usuariolbl = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setLocationByPlatform(true);
-        setResizable(false);
 
         jLabel1.setFont(new java.awt.Font("Dialog", 0, 36)); // NOI18N
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
@@ -61,18 +44,11 @@ public class bienvenida extends javax.swing.JFrame {
 
         Usuariolbl.setFont(new java.awt.Font("Dialog", 0, 18)); // NOI18N
         Usuariolbl.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        Usuariolbl.setText(this.user);
+        Usuariolbl.setText("usuario");
 
         jLabel3.setText("Ufff! Parece que este proyecto se merece un 10 :v");
 
         jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/pepe_label.jpg"))); // NOI18N
-
-        jButton1.setText("Salir");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
-            }
-        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -82,8 +58,7 @@ public class bienvenida extends javax.swing.JFrame {
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.CENTER)
                     .addComponent(jLabel3)
-                    .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jButton1))
+                    .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addContainerGap())
             .addGroup(layout.createSequentialGroup()
                 .addGap(97, 97, 97)
@@ -105,17 +80,11 @@ public class bienvenida extends javax.swing.JFrame {
                 .addComponent(jLabel4)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jLabel3)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jButton1)
-                .addContainerGap(34, Short.MAX_VALUE))
+                .addContainerGap(68, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        dispose();
-    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -154,7 +123,6 @@ public class bienvenida extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel Usuariolbl;
-    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
